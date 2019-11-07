e89db112ef8168fe2f273027e660a754b6348424
songziquan644 <644835157@qq.com>
2019-11-07 15:20:26 +0800

    yls

--- pages/techPage/techPage.js
+++ pages/techPage/techPage.js
@@ -23,7 +23,8 @@ Page({
 	 * 生命周期函数--监听页面加载
 	 */
 	onLoad: function (options) {
-		this.setData({teachid:options.teachid})
+		console.log(options)
+		this.setData({ teachid: options.userId})
 	},
 
 	/**
